1294cb3b33a1fa3f2a536f95823755ef6bb8728b
Liam Duffy <liamdanielduffy@gmail.com>
2023-09-28 09:24:57 -0400

    dog

--- src/components/canvas/View.tsx
+++ src/components/canvas/View.tsx
@@ -3,6 +3,7 @@
 import { forwardRef, Suspense, useImperativeHandle, useRef } from 'react'
 import { OrbitControls, PerspectiveCamera, View as ViewImpl } from '@react-three/drei'
 import { Three } from '@/helpers/components/Three'
+import dynamic from 'next/dynamic'
 
 export const Common = ({ color }) => (
   <Suspense fallback={null}>
@@ -20,7 +21,7 @@ interface ViewProps {
   [key: string]: any
 }
 
-const View = forwardRef(({ children, orbit, ...props }: ViewProps, ref) => {
+export const View = forwardRef(({ children, orbit, ...props }: ViewProps, ref) => {
   const localRef = useRef(null)
   useImperativeHandle(ref, () => localRef.current)
 
@@ -38,4 +39,36 @@ const View = forwardRef(({ children, orbit, ...props }: ViewProps, ref) => {
 })
 View.displayName = 'View'
 
-export { View }
+export function loadView() {
+  return dynamic<any>(() => import('@/components/canvas/View').then((mod) => mod.View), {
+    ssr: false,
+    loading: () => (
+      <div className='flex h-96 w-full flex-col items-center justify-center'>
+        <svg className='-ml-1 mr-3 h-5 w-5 animate-spin text-black' fill='none' viewBox='0 0 24 24'>
+          <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4' />
+          <path
+            className='opacity-75'
+            fill='currentColor'
+            d='M4 12a8 8 0 0 1 8-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 0 1 4 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+          />
+        </svg>
+      </div>
+    ),
+  })
+}
+
+// export const View = dynamic<any>(() => import('@/components/canvas/View').then((mod) => mod._View), {
+//   ssr: false,
+//   loading: () => (
+//     <div className='flex h-96 w-full flex-col items-center justify-center'>
+//       <svg className='-ml-1 mr-3 h-5 w-5 animate-spin text-black' fill='none' viewBox='0 0 24 24'>
+//         <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4' />
+//         <path
+//           className='opacity-75'
+//           fill='currentColor'
+//           d='M4 12a8 8 0 0 1 8-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 0 1 4 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+//         />
+//       </svg>
+//     </div>
+//   ),
+// })
